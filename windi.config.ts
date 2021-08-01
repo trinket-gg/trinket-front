b350cde5a79c1e78d48d83392814a4e49cb9f176
Antoine Lelong <azenoxe@gmail.com>
2021-08-01 20:24:30 +0200

    feat: add colors base trinket

--- windi.config.ts
+++ windi.config.ts
@@ -1,3 +1,18 @@
-export default {
-  attributify: true
-}
+import { defineConfig } from 'vite-plugin-windicss'
+
+export default defineConfig({
+  attributify: true,
+  theme: {
+    extend: {
+      colors: {
+        'tkt-black': '#131825',
+        'tkt-black-bg': '#1C212E',
+        'tkt-black-bg-light': '#262A36',
+        'tkt-black-bg-hover': '#334155',
+        'tkt-primary': '#52DFFF',
+        'tkt-text-primary': '#F8FAFC',
+        'tkt-text-secondary': '#94A3B8',
+      }
+    }
+  }
+})
